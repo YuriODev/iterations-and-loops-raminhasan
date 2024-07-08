54a51a89aa3ea9669a0e85b92331e840afd3c81b
raminhasan <ramin.hasan@outlook.com>
2024-07-08 17:16:51 +0000

    sol 20-23

--- exercises/exercise_20.py
+++ exercises/exercise_20.py
@@ -1,5 +1,12 @@
 # Your solution to Exercise 20
 
-start = 1
-end = 90
+endpt = int(input())
+
+for i in range(1, endpt + 1):
+  if i % 2 != 0:
+    print(i)
+
+  else:
+    pass
+
 stop = int(input()) 
--- exercises/exercise_22.py
+++ exercises/exercise_22.py
@@ -1,1 +1,11 @@
 # Your solution to Exercise 22
+
+input = int(input())
+
+while input > 0:
+  print(input)
+  input //= 10
+
+else:
+  print(input)
+ 
