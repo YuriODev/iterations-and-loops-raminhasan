4583d4b7a65ea2c0e8d1c9d48e31d53681e92901
raminhasan <ramin.hasan@outlook.com>
2024-07-08 11:06:13 +0000

    sol18-20

--- exercises/exercise_18.py
+++ exercises/exercise_18.py
@@ -1,1 +1,10 @@
 # Your solution to Exercise 18
+
+days = int(input())
+total = 0
+
+for _ in range(days):
+  errors = int(input())
+  total += errors
+
+print(total)
--- exercises/exercise_20.py
+++ exercises/exercise_20.py
@@ -1,1 +1,4 @@
 # Your solution to Exercise 20
+
+start = 1
+stop = int(input())
